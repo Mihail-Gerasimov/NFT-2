78e3c070909294b52d344ead2cb8d7b534bb1545
Mihail <mihailgerasimov86@gmail.com>
2022-07-29 17:58:26 +0400

    pizdec

--- js/main.js
+++ js/main.js
@@ -28,38 +28,50 @@ let select = function () {
 
 select();
 
-const slider = document.querySelector('.slider');
-const sl = new Swiper(slider, {
-    loop: true,
-    speed: 4000,
-    slidesPerView: '7.5',
-    autoplay: {
-        enabled: true,
-        delay: 0,
-    },
-    breakpoints: {
-        320: {
-            spaceBeetwen: 4,
-            slidesPerView: '3.9',
-        },
-        // when window width is >= 575px
-        575: {
-            spaceBeetwen: 8,
-            slidesPerView: '4.7',
-        },
-        // when window width is >= 767px
-        767: {
-            slidesPerView: '5.7',
-            spaceBeetwen: 12,
-        },
-        // when window width is >= 991px
-        991: {
-            slidesPerView: '6.5',
-            spaceBeetwen: 16,
-        },
-        // when window width is >= 1200px
-        1200: {
-            slidesPerView: '8.5',
-        }
-    }
-});
+const menuMob = document.querySelector('.burger');
+const cLosed = document.querySelector('.closed');
+const menuBlock = document.querySelector('.wrapper-list');
+
+menuMob.onclick = function () {
+    menuBlock.classList.toggle('wrapper-list__active');
+};
+
+cLosed.onclick = function () {
+    menuBlock.classList.remove('wrapper-list__active');
+};
+
+// const slider = document.querySelector('.slider');
+// const sl = new Swiper(slider, {
+//     loop: true,
+//     speed: 4000,
+//     slidesPerView: '7.5',
+//     autoplay: {
+//         enabled: true,
+//         delay: 0,
+//     },
+//     breakpoints: {
+//         320: {
+//             spaceBeetwen: 4,
+//             slidesPerView: '3.9',
+//         },
+//         // when window width is >= 575px
+//         575: {
+//             spaceBeetwen: 8,
+//             slidesPerView: '4.7',
+//         },
+//         // when window width is >= 767px
+//         767: {
+//             slidesPerView: '5.7',
+//             spaceBeetwen: 12,
+//         },
+//         // when window width is >= 991px
+//         991: {
+//             slidesPerView: '6.5',
+//             spaceBeetwen: 16,
+//         },
+//         // when window width is >= 1200px
+//         1200: {
+//             slidesPerView: '8.5',
+//         }
+//     }
+// });
